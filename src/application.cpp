8dff8bde63d5997cb73180b4433ebf16e1bd8b0d
Airalone <suppakit@hotmail.com>
2016-09-25 19:14:16 +0700

    Add command and process to control

--- src/application.cpp
+++ src/application.cpp
@@ -1,87 +1,176 @@
 #include <user_config.h>
 #include <SmingCore/SmingCore.h>
+#include "Bounce.h"
 
-// If you want, you can define WiFi settings globally in Eclipse Environment Variables
+#define WIFI_CONN_TIMEOUT   20
+
 #ifndef WIFI_SSID
-    #define WIFI_SSID "ABCam" // Put you SSID and Password here
+    #define WIFI_SSID "ABCam"
     #define WIFI_PWD "12345678"
 #endif
 
+#define SELECT_MODE_PIN     4
+#define RELEASE_PIN         5
+#define LEDSTS_PIN          8
+
+#define DEBOUNCE_TIME       5
+
+
 Timer procTimer;
-int sensorValue = 0;
+Timer procTimer2;
+Timer procTimer3;
 HttpClient sjCam;
 
-void onDataSent(HttpClient& client, bool successful)
-{
-    if (successful)
-        Serial.println("Success sent");
-    else
-        Serial.println("Failed");
+Bounce modeBtnBouncer    = Bounce(SELECT_MODE_PIN, DEBOUNCE_TIME);
+Bounce releaseBtnBouncer = Bounce(RELEASE_PIN, DEBOUNCE_TIME);
 
-    String response = client.getResponseString();
-    Serial.println("Server response: '" + response + "'");
-    if (response.length() > 0)
+typedef enum {
+    MODE_VIDEO = 0,
+    MODE_PHOTO = 1
+} SJCamMode_t;
+
+typedef enum {
+    CMD_GOTO_VIDEO_MODE = 0,
+    CMD_GOTO_PHOTO_MODE = 1,
+    CMD_START_REC_VIDEO = 2,
+    CMD_STOP_REC_VIDEO  = 3,
+    CMD_TAKE_A_PHOTO    = 4,
+} SJCamCommand_t;
+
+typedef struct {
+    SJCamCommand_t cmd;
+    String cmdNumber;
+    String param;
+} SJCamCmdMap_t;
+
+static const SJCamCmdMap_t SJCamCmdMapping[] {
+    { CMD_GOTO_VIDEO_MODE, "3001", "1" },
+    { CMD_GOTO_PHOTO_MODE, "3001", "0" },
+    { CMD_START_REC_VIDEO, "2001", "1" },
+    { CMD_STOP_REC_VIDEO,  "2001", "0" },
+    { CMD_TAKE_A_PHOTO,    "1001", "0" },
+};
+
+class SJCamRemote {
+public :
+    SJCamRemote()
     {
-        int intVal = response.toInt();
-
-        if (intVal == 0)
-            Serial.println("Sensor value wasn't accepted. May be we need to wait a little?");
+        currentMode = MODE_VIDEO;
     }
-}
+    ~SJCamRemote()
+    {
+        currentMode = MODE_VIDEO;
+    }
 
-void sendData()
+    SJCamMode_t getCurrentMode () { return currentMode; }
+    void setCurrentMode (SJCamMode_t mode) { currentMode = mode; }
+
+    void sendCommand (SJCamCommand_t cmd)
+    {
+        if (client.isProcessing())
+            return;
+
+        client.downloadString("http://192.168.1.254/?custom=1&cmd=" + SJCamCmdMapping[cmd].cmdNumber +
+                                                            "&par=" + SJCamCmdMapping[cmd].param,
+                              onSentResponse);
+    }
+
+private :
+    HttpClient client;
+    SJCamMode_t currentMode = MODE_VIDEO;
+
+    static void onSentResponse(HttpClient& client, bool successful) {
+        if (successful)
+            Serial.println("Success sent command");
+        else
+            Serial.println("Failed command");
+    }
+};
+
+bool isVideoRecording = false;
+SJCamRemote sjcam;
+
+void processModeBtn()
 {
-    static int flag = 0;
-    if (sjCam.isProcessing()) return; // We need to wait while request processing was completed
-
-    // Read our sensor value :)
-    sensorValue++;
-
-    if (flag == 0) {
-        flag = 1;
-        Serial.println("Go to photo mode");
-        sjCam.downloadString("http://192.168.1.254/?custom=1&cmd=3001&par=0", onDataSent);
+    Serial.println(__func__);
+    if (sjcam.getCurrentMode() == MODE_PHOTO) {
+        Serial.println(CMD_GOTO_VIDEO_MODE);
+        sjcam.sendCommand (CMD_GOTO_VIDEO_MODE);
+        sjcam.setCurrentMode (MODE_VIDEO);
     }
-    else {
-        flag = 0;
-        Serial.println("Go to video mode");
-        sjCam.downloadString("http://192.168.1.254/?custom=1&cmd=3001&par=1", onDataSent);
-    }
+    else if (sjcam.getCurrentMode() == MODE_VIDEO) {
+        if (isVideoRecording) {
+            sjcam.sendCommand (CMD_STOP_REC_VIDEO);
+        }
 
+        Serial.println(CMD_GOTO_PHOTO_MODE);
+        sjcam.sendCommand (CMD_GOTO_PHOTO_MODE);
+        sjcam.setCurrentMode (MODE_PHOTO);
+    }
 }
 
-// Will be called when WiFi station was connected to AP
+void processReleaseBtn()
+{
+    Serial.println(__func__);
+    if (sjcam.getCurrentMode() == MODE_PHOTO) {
+        Serial.println("CMD_TAKE_A_PHOTO");
+        sjcam.sendCommand (CMD_TAKE_A_PHOTO);
+    }
+    else if (sjcam.getCurrentMode() == MODE_VIDEO) {
+        if (!isVideoRecording) {
+            Serial.println("CMD_START_REC_VIDEO");
+            sjcam.sendCommand (CMD_START_REC_VIDEO);
+        }
+        else {
+            Serial.println("CMD_STOP_REC_VIDEO");
+            sjcam.sendCommand (CMD_STOP_REC_VIDEO);
+        }
+    }
+}
+
+void processButton()
+{
+    if (modeBtnBouncer.update()) {
+        if (modeBtnBouncer.read() == HIGH) {
+            procTimer.initializeMs(1, processModeBtn).start(false);
+        }
+    }
+
+    if (releaseBtnBouncer.update()) {
+        if (releaseBtnBouncer.read() == HIGH) {
+            procTimer.initializeMs(1, processReleaseBtn).start(false);
+        }
+    }
+}
+
 void connectOk()
 {
-    Serial.println("I'm CONNECTED");
-
-    // Start send data loop
-    procTimer.initializeMs(10 * 1000, sendData).start(); // every 10 seconds
+    Serial.println("CONNECTED");
 }
 
-// Will be called when WiFi station timeout was reached
 void connectFail()
 {
-    Serial.println("I'm NOT CONNECTED. Need help :(");
-
-    // Start soft access point
-    WifiAccessPoint.enable(true);
-    WifiAccessPoint.config("CONFIG ME PLEEEEASE...", "", AUTH_OPEN);
-
-    // .. some you code for configuration ..
+    Serial.println("CANNOT CONNECT WIFI.");
 }
 
 void init()
 {
-    spiffs_mount(); // Mount file system, in order to work with files
+    //spiffs_mount(); // Mount file system, in order to work with files
 
-    Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
-    Serial.systemDebugOutput(false); // Disable debug output to serial
+    /* Uart init; 115200 and no system debug */
+    Serial.begin(SERIAL_BAUD_RATE);
+    Serial.systemDebugOutput(false);
 
+    /* Button init */
+    pinMode(SELECT_MODE_PIN,INPUT);
+    pinMode(RELEASE_PIN,OUTPUT);
+
+    procTimer3.initializeMs(50, processButton).start();
+
+    /* Start Wifi */
     WifiStation.config(WIFI_SSID, WIFI_PWD);
     WifiStation.enable(true);
     WifiAccessPoint.enable(false);
 
-    // Run our method when station was connected to AP (or not connected)
-    WifiStation.waitConnection(connectOk, 20, connectFail); // We recommend 20+ seconds for connection timeout at start
+    WifiStation.waitConnection(connectOk, WIFI_CONN_TIMEOUT, connectFail);
 }
