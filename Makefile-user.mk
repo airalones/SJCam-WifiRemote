10f9c84b32e318f1a944b629860d71503efbaafd
Airalone <suppakit@hotmail.com>
2016-09-29 19:53:00 +0700

    Fix makefile configuration

--- Makefile-user.mk
+++ Makefile-user.mk
@@ -1,43 +1,18 @@
-## Local build configuration
-## Parameters configured here will override default and ENV values.
-## Uncomment and change examples:
+#export ESP_MODULE := ESP12E 
+export ESP_MODULE := ESP13
 
-#Add your source directories here separated by space
 MODULES = src lib
 
-## ESP_HOME sets the path where ESP tools and SDK are located.
-## Windows:
-# ESP_HOME = c:/Espressif
+#SPIFF_FILES = files
 
-## MacOS / Linux:
-#ESP_HOME = /opt/esp-open-sdk
-
-## SMING_HOME sets the path where Sming framework is located.
-## Windows:
-# SMING_HOME = c:/tools/sming/Sming 
-
-# MacOS / Linux
-# SMING_HOME = /opt/sming/Sming
-
-## COM port parameter is reqruied to flash firmware correctly.
-## Windows: 
-#COM_PORT = COM5
-COM_PORT = COM4
-
-
-# MacOS / Linux:
-# COM_PORT = /dev/tty.usbserial
-
-# Com port speed
+COM_PORT	= COM5
 COM_SPEED	= 230400
+SPI_SPEED   = 40
 
-## Flash parameters
-# SPI_SPEED = 40, 26, 20, 80
-SPI_SPEED ?= 40
-# SPI_MODE: qio, qout, dio, dout
-SPI_MODE ?= qio
-# SPI_SIZE: 512K, 256K, 1M, 2M, 4M
-SPI_SIZE ?= 1M
-#SPI_SIZE ?= 4M
-
-SPIFF_FILES = files
+ifeq ($ESP_MODULE, ESP13)
+	SPI_MODE  = qio
+	SPI_SIZE  = 1M
+else
+	SPI_MODE  = dio
+	SPI_SIZE  = 4M
+endif
